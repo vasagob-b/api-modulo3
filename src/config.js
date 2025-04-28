e6f3d8bbbf02abda1b28820a3766d86bd1196f35
vasagob-b <patricio.barra09086@edu.ipchile.cl>
2025-04-28 00:28:10 -0400

    Conectar a Railway

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-import {config} from 'dotenv'
-config()
+import { config } from 'dotenv';
+config();
 
-export const PORT = process.env.PORT || 3000
-export const DB_USER = process.env.DB_USER || 'sql10775453'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'XjNXiTSA3r'
-export const DB_HOST = process.env.DB_HOST || 'sql10.freesqldatabase.com'
-export const DB_DATABASE = process.env.DB_DATABASE || 'sql10775453'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const PORT = process.env.PORT || 3000;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PORT = process.env.DB_PORT || 3306;
